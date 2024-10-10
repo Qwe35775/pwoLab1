a4476b2fce3cd89931831003ba8ff0b0f15bf010
Qwe35775 <dawid.smarzak@o2.pl>
2024-10-10 19:07:35 +0200

    commit 6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome11!!");
-        System.out.printf("Hello and welcome11!!");
-        System.out.printf("Hello and welcome11!!");
-        System.out.printf("Hello and welcome11!!");
-        System.out.printf("Hello and welcome11!!");
-        System.out.printf("Hello and welcome11!jdfhbnfkhv!");
+        System.out.print("Hello and welcome11!!");
+        System.out.print("Hello and welcome11!!");
+        System.out.print("Hello and welcome11!!");
+        System.out.print("Hello and welcome11!!");
+        System.out.print("Hello and welcome11!!");
+        System.out.print("Hello and welcome11!jdfhbnfkhv!");
 
-        for (int i = 1; i <= 5; i++)  System.out.println("i = " + i);
-        }
+        for (int i = 1; i <= 5; i++) System.out.println("i = " + i);
     }
+}
 }
