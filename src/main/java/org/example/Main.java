f862f77b5839421470120cf188cc22f0872656ea
Qwe35775 <dawid.smarzak@o2.pl>
2024-10-10 19:00:18 +0200

    commit4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,9 @@ public class Main {
         System.out.printf("Hello and welcome11!!");
         System.out.printf("Hello and welcome11!!");
         System.out.printf("Hello and welcome11!!");
+        System.out.printf("Hello and welcome11!!");
+        System.out.printf("Hello and welcome11!!");
+        System.out.printf("Hello and welcome11!!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
