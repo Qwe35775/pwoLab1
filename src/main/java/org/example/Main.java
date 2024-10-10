e89023dbbc432b0a672c80cd16071d2e764462a0
Qwe35775 <dawid.smarzak@o2.pl>
2024-10-10 19:16:35 +0200

    commit 10
    commit 8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,10 @@ public class Main {
         System.out.print("Hello and welcome11!!");
         System.out.print("Hello and welcome11!!");
         System.out.print("Hello and welcome11!jdfhbnfkhv!");
-
+        System.out.print("Hello and welcome11!jdfhbnfkhv!");
+        System.out.print("Hello and welcome11!jdfhbnfkhv!");
         for (int i = 1; i <= 5; i++) System.out.println("i = " + i);
+        System.out.print("Hello and welcome11!jdfhbnfkhv!");
+        System.out.print("Hello and welcome11!jdfhbnfkhv!");
     }
 }
