62cfe49879f41c98bf344cfdc5a25881f072cf03
Qwe35775 <dawid.smarzak@o2.pl>
2024-10-10 19:47:42 +0200

    commit 14

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,5 +13,7 @@ public class Main {
         System.out.print("Hello and welcome11!jdfhbnfkhv!");
         System.out.print("Hello and welcome11!jdfhbnfkhv!");
         for (int i = 1; i <= 5; i++) System.out.println("i = " + i);
+        System.out.print("Hello and welcome11!jdfhbnfkhv!");
+        System.out.print("Hello and welcome11!jdfhbnfkhv!");
     }
 }
